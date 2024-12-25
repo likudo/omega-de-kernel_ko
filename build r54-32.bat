466a04b4b7327ca77778fbcce107c5490be9cc30
r35 <likudo@naver.com>
2024-12-25 20:25:43 +0900

    devKit 세팅 관련 메모
    원본 리포는 r53 으로 빌드 셋업되어있으나 순정 r53 으로 빌드 시도시 실패함
    (r53 백업 배포파일에 rules 나 gba.specs 등 다수의 파일이 누락되어있음)
    오메가 버전인 r47 로는 DE 도 빌드를 성공하지만 현재 DE 가 없어서 테스트를 할 수 없는 상황이고 구버전 빌드의 찝찝한 감이 있으므로
    r47 에 r54-3 을 덮어씌워서 억지로 업그레이드 하는 방식으로 일단 해결
    (r54 가 마지막 win32 버전이고 이후로는 86_64 로 올라갔음)

---
+++ build r54-32.bat
@@ -0,0 +1,7 @@
+set PATH=C:\devkitPro\msys\bin;C:\devkitPro\devkitARM_r54\bin;%PATH%
+set DEVKITARM=/c/devkitPro/devkitARM_r54
+set DEVKITPRO=/c/devkitPro
+set LIBGBA=/c/devkitPro/libgba
+
+make  
+pause
